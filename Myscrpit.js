17866ec0b18dffcb812e7cede1c3eac24e8fd863
Gowtham-A-M <153898377+Gowtham-A-M@users.noreply.github.com>
2025-02-03 23:41:50 +0530

    Fontend

---
+++ Myscrpit.js
@@ -0,0 +1,20 @@
+
+// function showCourse() {
+//     fetch('http://localhost:8080/Course')
+//         .then(response => response.json())
+//         .then(courses => {
+//             const courseTable = document.getElementById("availablecourse");
+//             courses.forEach(course => {
+//                 const row = document.createElement('tr');
+//                 row.innerHTML = `
+//                     <td>${course.courseId}</td>
+//                     <td>${course.courseName}</td>
+//                     <td>${course.trainer}</td>
+//                 `;
+//                 courseTable.appendChild(row);
+//             });
+//         })
+//         .catch(error => console.log(error));
+// }
+
+
